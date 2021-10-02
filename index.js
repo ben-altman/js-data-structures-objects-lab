c7f8896f04960ea20cf9a9d28cc81adbce1aaae7
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-02 19:33:49 -0400

    commit tree b1c3cc68ce4f4aa2e8304c93ad65baf57e5b0879

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    const newDriver = Object.assign({}, obj, {[key]: value});
+    return newDriver;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign(obj, {[key]: value});
+};
+
+function deleteFromDriverByKey(obj, key) {
+    const keylessDriver = Object.assign({}, obj);
+    delete keylessDriver[key];
+    return keylessDriver;
+};
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
